3d5f5dd9ca0353cf61017a4f2a8291e5e2f33d64
kumar-balu <147791705+kumar-balu@users.noreply.github.com>
2023-10-20 09:58:40 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);     z=(5*x);     if (z>=y)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Reach_Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=(5*x);
+    if (z>=y)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+}
